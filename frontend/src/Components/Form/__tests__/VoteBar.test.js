992e13a265161c86d09f7d354a7ff3f5e419e6a4
Administrator <sk@unelma.ee>
2022-03-24 07:45:29 +0200

    Minor cleanup

--- frontend/src/Components/Form/__tests__/VoteBar.test.js
+++ frontend/src/Components/Form/__tests__/VoteBar.test.js
@@ -1,53 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import VoteBar from "../VoteBar";
-// import Enzyme, { shallow } from "enzyme";
-// import Adapter from "enzyme-adapter-react-16";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VoteBar from '../VoteBar';
 
-import { cleanup, fireEvent, render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-
-// Enzyme.configure({ adapter: new Adapter() });
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 afterEach(cleanup);
 
-describe("<VoteBar/> Component", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
+describe('<VoteBar/> Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
     ReactDOM.render(<VoteBar />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it("Voting correctly", () => {
+  it('Voting correctly', () => {
     const { container } = render(<VoteBar score={10} />);
-    const input = container.querySelector("input");
-    fireEvent.change(input, { target: { value: "2" } });
-    fireEvent.change(input, { target: { value: "5" } });
-    expect(input.value).toEqual("5");
+    const input = container.querySelector('input');
+    fireEvent.change(input, { target: { value: '2' } });
+    fireEvent.change(input, { target: { value: '5' } });
+    expect(input.value).toEqual('5');
   });
 });
-
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import Form from "../Form";
-// import { cleanup, render } from "@testing-library/react";
-// import "@testing-library/jest-dom/extend-expect";
-
-
-// afterEach(cleanup);
-
-// describe("<Form/> Component", () => {
-//   it("renders without crashing", () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<Form />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-
-//   it("Voting correctly", () => {
-//     const { container } = render(<Form />);
-
-//     const tree = component.toJSON();
-//     expect(tree.props["data-testid"]).toEqual("submit-form");
-//   });
-// });
